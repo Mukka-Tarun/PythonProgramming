a8c4d7891080d89b58c474e87cba8c94f59f5640
Mukka Tarun <94466484+Mukka-Tarun@users.noreply.github.com>
2023-11-30 12:18:51 +0530

    Add files via upload

---
+++ CountDigits.py
@@ -0,0 +1,15 @@
+#Python Program to Count the Number of Digits Present In a Number
+
+#Example 1: Count Number of Digits in an Integer using while loop
+num = 3452
+count = 0
+
+while num != 0:
+    num //= 10
+    count += 1
+
+print("Number of digits: " + str(count))
+
+#Example 2: Using inbuilt methods
+num = 123456
+print(len(str(num)))
