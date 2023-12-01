52f8287a009c1e5f835aad9b4cd621f8aea2b913
Mukka Tarun <94466484+Mukka-Tarun@users.noreply.github.com>
2023-12-01 22:39:12 +0530

    Add files via upload

---
+++ CapFirstLetter.py
@@ -0,0 +1,12 @@
+#Python Program to Capitalize the First Character of a String
+
+#Example 1: Using list slicing
+my_string = "Tarun is Lit"
+print(my_string[0].upper() + my_string[1:])
+
+#Example 2: Using inbuilt method capitalize()
+my_string = "Tarun is dope"
+
+cap_string = my_string.capitalize()
+
+print(cap_string)
